43bb8031a3c76b8574e1c11a9cb725b7c870d17c
jpmoraess <moraess.it@gmail.com>
2022-07-04 20:58:46 -0300

    testing webflux endpoint using @WebFluxTest annotation

---
+++ movies-info-service/src/test/java/unit/br/com/moraesit/movies/info/service/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,27 @@
+package br.com.moraesit.movies.info.service.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest(controllers = FluxAndMonoController.class)
+@AutoConfigureWebTestClient
+class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void flux() {
+        webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .hasSize(3);
+    }
+}
