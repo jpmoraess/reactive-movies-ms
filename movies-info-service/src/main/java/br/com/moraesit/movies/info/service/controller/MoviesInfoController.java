edd2e80b606a8c0b1c555b20d16c63b60cb51b57
jpmoraess <moraess.it@gmail.com>
2022-07-05 08:23:51 -0300

    build a POST endpoint to create a new MovieInfo

---
+++ movies-info-service/src/main/java/br/com/moraesit/movies/info/service/controller/MoviesInfoController.java
@@ -0,0 +1,24 @@
+package br.com.moraesit.movies.info.service.controller;
+
+import br.com.moraesit.movies.info.service.domain.MovieInfo;
+import br.com.moraesit.movies.info.service.service.MoviesInfoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1")
+public class MoviesInfoController {
+
+    private final MoviesInfoService moviesInfoService;
+
+    public MoviesInfoController(MoviesInfoService moviesInfoService) {
+        this.moviesInfoService = moviesInfoService;
+    }
+
+    @PostMapping("/movieinfos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
+        return moviesInfoService.addMovieInfo(movieInfo);
+    }
+}
---
+++ movies-info-service/src/main/java/br/com/moraesit/movies/info/service/service/MoviesInfoService.java
@@ -0,0 +1,22 @@
+package br.com.moraesit.movies.info.service.service;
+
+import br.com.moraesit.movies.info.service.domain.MovieInfo;
+import br.com.moraesit.movies.info.service.repository.MovieInfoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Service
+public class MoviesInfoService {
+
+    private final MovieInfoRepository movieInfoRepository;
+
+    public MoviesInfoService(MovieInfoRepository movieInfoRepository) {
+        this.movieInfoRepository = movieInfoRepository;
+    }
+
+    public Mono<MovieInfo> addMovieInfo(MovieInfo movieInfo) {
+        return movieInfoRepository.save(movieInfo);
+    }
+}
