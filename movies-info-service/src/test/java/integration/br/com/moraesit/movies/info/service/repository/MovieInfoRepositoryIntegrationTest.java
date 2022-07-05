dd8320036986f1840241aa73f6fd389d95a6f999
jpmoraess <moraess.it@gmail.com>
2022-07-04 21:52:05 -0300

    write integration test for findAll() MovieInfo document

--- movies-info-service/src/test/java/integration/br/com/moraesit/movies/info/service/repository/MovieInfoRepositoryIntegrationTest.java
+++ movies-info-service/src/test/java/integration/br/com/moraesit/movies/info/service/repository/MovieInfoRepositoryIntegrationTest.java
@@ -1,14 +1,54 @@
 package br.com.moraesit.movies.info.service.repository;
 
+import br.com.moraesit.movies.info.service.domain.MovieInfo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @DataMongoTest
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
 @ActiveProfiles("test")
 class MovieInfoRepositoryIntegrationTest {
 
     @Autowired
     MovieInfoRepository movieInfoRepository;
 
+    @BeforeEach
+    void setUp() {
+        var moviesInfo = List.of(
+                new MovieInfo(null, "Batman Begins", 2005,
+                        List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+
+                new MovieInfo(null, "The Dark Knight", 2008,
+                        List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+
+                new MovieInfo(null, "Dark Knight Rises", 2012,
+                        List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2012-07-20"))
+        );
+
+        movieInfoRepository.saveAll(moviesInfo)
+                .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void findAll() {
+        var moviesInfoFlux = movieInfoRepository.findAll().log();
+
+        StepVerifier.create(moviesInfoFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
 }
