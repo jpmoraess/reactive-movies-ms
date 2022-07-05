d060c83aac0a2fc601251b2e0f7415ae1eaa9be5
jpmoraess <moraess.it@gmail.com>
2022-07-05 09:47:16 -0300

    unit test for getById endpoint - GET

--- movies-info-service/src/test/java/unit/br/com/moraesit/movies/info/service/controller/MoviesInfoControllerUnitTest.java
+++ movies-info-service/src/test/java/unit/br/com/moraesit/movies/info/service/controller/MoviesInfoControllerUnitTest.java
@@ -9,10 +9,13 @@ import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @WebFluxTest(controllers = MoviesInfoController.class)
@@ -50,5 +53,27 @@ public class MoviesInfoControllerUnitTest {
                 .is2xxSuccessful()
                 .expectBodyList(MovieInfo.class)
                 .hasSize(3);
+    }
+
+    @Test
+    void getMovieById() {
+        var movieInfoId = "abc";
+        var movieInfo = new MovieInfo("abc", "Dark Knight Rises", 2012,
+                List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2012-07-20"));
+
+        when(moviesInfoServiceMock.getMovieInfoById(movieInfoId)).thenReturn(Mono.just(movieInfo));
+
+        webTestClient
+                .get()
+                .uri(MOVIE_INFOS_URL + "/{id}", movieInfoId)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(MovieInfo.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+                    var movieInfo1 = movieInfoEntityExchangeResult.getResponseBody();
+                    assertNotNull(movieInfo1);
+                    assertEquals("Dark Knight Rises", movieInfo1.getName());
+                });
     }
 }
