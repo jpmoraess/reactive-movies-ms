440d5f54b6a578d688f3e8b4c5e3446fd0d14bc8
jpmoraess <moraess.it@gmail.com>
2022-07-05 09:32:22 -0300

    setting up the unit test in WebFlux

---
+++ movies-info-service/src/test/java/unit/br/com/moraesit/movies/info/service/controller/MoviesInfoControllerUnitTest.java
@@ -0,0 +1,19 @@
+package br.com.moraesit.movies.info.service.controller;
+
+import br.com.moraesit.movies.info.service.service.MoviesInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest(controllers = MoviesInfoController.class)
+@AutoConfigureWebTestClient
+public class MoviesInfoControllerUnitTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private MoviesInfoService moviesInfoServiceMock;
+}
