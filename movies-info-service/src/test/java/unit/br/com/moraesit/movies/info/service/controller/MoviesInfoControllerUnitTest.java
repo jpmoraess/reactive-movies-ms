f6bb678bd47509b8486c8ae9c20a225a025b8d90
jpmoraess <moraess.it@gmail.com>
2022-07-05 09:38:51 -0300

    unit test for getAllMoviesInfo endpoint - GET

--- movies-info-service/src/test/java/unit/br/com/moraesit/movies/info/service/controller/MoviesInfoControllerUnitTest.java
+++ movies-info-service/src/test/java/unit/br/com/moraesit/movies/info/service/controller/MoviesInfoControllerUnitTest.java
@@ -1,11 +1,19 @@
 package br.com.moraesit.movies.info.service.controller;
 
+import br.com.moraesit.movies.info.service.domain.MovieInfo;
 import br.com.moraesit.movies.info.service.service.MoviesInfoService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @WebFluxTest(controllers = MoviesInfoController.class)
 @AutoConfigureWebTestClient
@@ -16,4 +24,31 @@ public class MoviesInfoControllerUnitTest {
 
     @MockBean
     private MoviesInfoService moviesInfoServiceMock;
+
+    static String MOVIE_INFOS_URL = "/v1/movieinfos";
+
+    @Test
+    void getAllMoviesInfo() {
+        var moviesInfo = List.of(
+                new MovieInfo(null, "Batman Begins", 2005,
+                        List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+
+                new MovieInfo(null, "The Dark Knight", 2008,
+                        List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+
+                new MovieInfo("abc", "Dark Knight Rises", 2012,
+                        List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2012-07-20"))
+        );
+
+        when(moviesInfoServiceMock.getAllMovieInfos()).thenReturn(Flux.fromIterable(moviesInfo));
+
+        webTestClient
+                .get()
+                .uri(MOVIE_INFOS_URL)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(MovieInfo.class)
+                .hasSize(3);
+    }
 }
