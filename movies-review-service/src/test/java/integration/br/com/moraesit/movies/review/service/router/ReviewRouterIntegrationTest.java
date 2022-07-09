1f22e1765d526f099638468b9e55f97317f98826
jpmoraess <moraess.it@gmail.com>
2022-07-08 22:12:18 -0300

    writing integration test for create a new Review

---
+++ movies-review-service/src/test/java/integration/br/com/moraesit/movies/review/service/router/ReviewRouterIntegrationTest.java
@@ -0,0 +1,72 @@
+package br.com.moraesit.movies.review.service.router;
+
+import br.com.moraesit.movies.review.service.domain.Review;
+import br.com.moraesit.movies.review.service.repository.ReviewReactiveRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ReviewRouterIntegrationTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ReviewReactiveRepository reviewReactiveRepository;
+
+    static String REVIEWS_URL = "/v1/reviews";
+
+    @BeforeEach
+    void setUp() {
+        var reviews = List.of(
+                new Review(null, 1L, "Awesome Movie", 9.0),
+                new Review(null, 2L, "Fantastic Movie", 8.5),
+                new Review(null, 3L, "Bad Movie", 3.0)
+        );
+
+        reviewReactiveRepository.saveAll(reviews).blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        reviewReactiveRepository.deleteAll().block();
+    }
+
+    @Test
+    void addReview() {
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+
+        webTestClient
+                .post()
+                .uri(REVIEWS_URL)
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewEntityExchangeResult -> {
+                    var savedReview = reviewEntityExchangeResult.getResponseBody();
+
+                    assertNotNull(savedReview);
+                    assertNotNull(savedReview.getReviewId());
+                    assertNotNull(savedReview.getMovieInfoId());
+                    assertEquals("Awesome Movie", savedReview.getComment());
+                    assertEquals(9.0, savedReview.getRating());
+                });
+    }
+}
