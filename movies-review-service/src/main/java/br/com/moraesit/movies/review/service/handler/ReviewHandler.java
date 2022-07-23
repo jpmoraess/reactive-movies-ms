f4032b636fa22a376b041929d5d265193616bdfb
jpmoraess <moraess.it@gmail.com>
2022-07-23 14:11:48 -0300

    validating the bean using validator

--- movies-review-service/src/main/java/br/com/moraesit/movies/review/service/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/br/com/moraesit/movies/review/service/handler/ReviewHandler.java
@@ -1,7 +1,9 @@
 package br.com.moraesit.movies.review.service.handler;
 
 import br.com.moraesit.movies.review.service.domain.Review;
+import br.com.moraesit.movies.review.service.exception.ReviewDataException;
 import br.com.moraesit.movies.review.service.repository.ReviewReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -9,19 +11,41 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Component
 public class ReviewHandler {
 
+    private final Validator validator;
+
     private final ReviewReactiveRepository reviewReactiveRepository;
 
-    public ReviewHandler(ReviewReactiveRepository reviewReactiveRepository) {
+    public ReviewHandler(Validator validator, ReviewReactiveRepository reviewReactiveRepository) {
+        this.validator = validator;
         this.reviewReactiveRepository = reviewReactiveRepository;
     }
 
     public Mono<ServerResponse> addReview(ServerRequest request) {
         return request.bodyToMono(Review.class)
+                .doOnNext(this::validate)
                 .flatMap(reviewReactiveRepository::save)
                 .flatMap(ServerResponse.status(HttpStatus.CREATED)::bodyValue);
+    }
+
+    private void validate(Review review) {
+        var constraintViolations = validator.validate(review);
+        log.info("constraintViolations: {}", constraintViolations);
+        if (constraintViolations.size() > 0) {
+            var errorMessage = constraintViolations
+                    .stream()
+                    .map(ConstraintViolation::getMessage)
+                    .sorted()
+                    .collect(Collectors.joining(","));
+            throw new ReviewDataException(errorMessage);
+        }
     }
 
     public Mono<ServerResponse> getReviews(ServerRequest request) {
