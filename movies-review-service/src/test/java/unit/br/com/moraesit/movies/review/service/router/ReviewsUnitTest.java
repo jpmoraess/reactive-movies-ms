d1c5ebcab100843d6477b081f6a095621d9c1084
jpmoraess <moraess.it@gmail.com>
2022-07-23 13:54:08 -0300

    adding more test cases

--- movies-review-service/src/test/java/unit/br/com/moraesit/movies/review/service/router/ReviewsUnitTest.java
+++ movies-review-service/src/test/java/unit/br/com/moraesit/movies/review/service/router/ReviewsUnitTest.java
@@ -10,10 +10,14 @@ import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
 
@@ -53,5 +57,65 @@ public class ReviewsUnitTest {
                     assertEquals("Awesome Movie", savedReview.getComment());
                     assertEquals(9.0, savedReview.getRating());
                 });
+    }
+
+    @Test
+    void getAllReviews() {
+        var reviewList = List.of(
+                new Review(null, 1L, "Awesome Movie", 9.0),
+                new Review(null, 1L, "Awesome Movie1", 9.0),
+                new Review(null, 2L, "Excellent Movie", 8.0));
+
+        when(reviewReactiveRepository.findAll()).thenReturn(Flux.fromIterable(reviewList));
+
+        webTestClient
+                .get()
+                .uri("/v1/reviews")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Review.class)
+                .value(reviews -> {
+                    assertEquals(3, reviews.size());
+                });
+    }
+
+    @Test
+    void updateReview() {
+        var reviewUpdate = new Review(null, 1L, "Not an Awesome Movie", 8.0);
+
+        when(reviewReactiveRepository.save(isA(Review.class)))
+                .thenReturn(Mono.just(new Review("abc", 1L, "Not an Awesome Movie", 8.0)));
+        when(reviewReactiveRepository.findById((String) any()))
+                .thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+
+        webTestClient
+                .put()
+                .uri("/v1/reviews/{id}", "abc")
+                .bodyValue(reviewUpdate)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Review.class)
+                .consumeWith(reviewResponse -> {
+                    var updatedReview = reviewResponse.getResponseBody();
+                    assert updatedReview != null;
+                    System.out.println("updatedReview : " + updatedReview);
+                    assertEquals(8.0, updatedReview.getRating());
+                    assertEquals("Not an Awesome Movie", updatedReview.getComment());
+                });
+    }
+
+    @Test
+    void deleteReview() {
+        var reviewId = "abc";
+        when(reviewReactiveRepository.findById((String) any()))
+                .thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+        when(reviewReactiveRepository.deleteById((String) any())).thenReturn(Mono.empty());
+
+        webTestClient
+                .delete()
+                .uri("/v1/reviews/{id}", reviewId)
+                .exchange()
+                .expectStatus().isNoContent();
     }
 }
