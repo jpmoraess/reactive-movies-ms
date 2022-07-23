917aae3433d5cd0a38c273fb20f36ea5315173c9
jpmoraess <moraess.it@gmail.com>
2022-07-23 13:48:10 -0300

    unit test the POST endpoint for creating a new Review

--- movies-review-service/src/test/java/unit/br/com/moraesit/movies/review/service/router/ReviewsUnitTest.java
+++ movies-review-service/src/test/java/unit/br/com/moraesit/movies/review/service/router/ReviewsUnitTest.java
@@ -1,13 +1,21 @@
 package br.com.moraesit.movies.review.service.router;
 
+import br.com.moraesit.movies.review.service.domain.Review;
 import br.com.moraesit.movies.review.service.handler.ReviewHandler;
 import br.com.moraesit.movies.review.service.repository.ReviewReactiveRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
 
 @WebFluxTest
 @ContextConfiguration(classes = {ReviewRouter.class, ReviewHandler.class})
@@ -20,5 +28,30 @@ public class ReviewsUnitTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    static String REVIEWS_URL = "/v1/reviews";
 
+    @Test
+    void addReview() {
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+
+        when(reviewReactiveRepository.save(isA(Review.class)))
+                .thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+
+        webTestClient
+                .post()
+                .uri(REVIEWS_URL)
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewEntityExchangeResult -> {
+                    var savedReview = reviewEntityExchangeResult.getResponseBody();
+                    assertNotNull(savedReview);
+                    assertNotNull(savedReview.getReviewId());
+                    assertNotNull(savedReview.getMovieInfoId());
+                    assertEquals("Awesome Movie", savedReview.getComment());
+                    assertEquals(9.0, savedReview.getRating());
+                });
+    }
 }
