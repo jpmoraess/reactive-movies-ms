a9e945587a2a0b9427df09a6476a93d4c61796a3
jpmoraess <moraess.it@gmail.com>
2022-07-23 13:39:08 -0300

    setting up test class for unit testing

---
+++ movies-review-service/src/test/java/unit/br/com/moraesit/movies/review/service/router/ReviewsUnitTest.java
@@ -0,0 +1,24 @@
+package br.com.moraesit.movies.review.service.router;
+
+import br.com.moraesit.movies.review.service.handler.ReviewHandler;
+import br.com.moraesit.movies.review.service.repository.ReviewReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest
+@ContextConfiguration(classes = {ReviewRouter.class, ReviewHandler.class})
+@AutoConfigureWebTestClient
+public class ReviewsUnitTest {
+
+    @MockBean
+    private ReviewReactiveRepository reviewReactiveRepository;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+
+}
